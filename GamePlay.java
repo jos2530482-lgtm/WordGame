37b9d2b6f3daaaa3102ca9f5f765d672598bf5c0
jos2530482-lgtm <jos2530482@maricopa.edu>
2026-09-12 01:13:25 -0700

    turn

--- GamePlay.java
+++ GamePlay.java
@@ -38,4 +38,3 @@ public class GamePlay {
         scanner.close();
     }
 }
-
--- Turn.java
+++ Turn.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 public class Turn {
+
+private int win = 5;
+private int lose = 5;
+
     public boolean takeTurn(Players player, Hosts host) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println(host.getNameFirst() + " " + host.getNameLast() +
-        " is taking their turn against " + player.getNameFirst() + " " + player.getNameLast());
+        ": " + player.getNameFirst() + " " + player.getNameLast());
+
         System.out.println("Please enter a number between 0 and 100.");
         int guess = scanner.nextInt();
+
         Numbers randomNumber = new Numbers();
-        randomNumber.generateNumber();
         boolean sameNumber = randomNumber.compareNumber(guess);
+
         if (sameNumber) {
-            System.out.println(player.getNameFirst() + " " + player.getNameLast() +
-            " has won the game!");
+            player.setMoney(player.getMoney() + win);
+            System.out.println(player);
             return true;
         } else {
-            System.out.println(player.getNameFirst() + " " + player.getNameLast() +
-            " has lost the game.");
+            player.setMoney(player.getMoney() - lose);
+            System.out.println(player);
             return false;
         }
     }
+}
