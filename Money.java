5a80928a03814e49315e6851532bdeb5bf9c5076
jos2530482-lgtm <jos2530482@maricopa.edu>
2026-09-18 19:12:34 -0700

    money

---
+++ Money.java
@@ -0,0 +1,15 @@
+public class Money implements Award {
+    private int win = 50;
+    private int lose = 50;
+
+    @Override
+    public int displayWinnings(Players player, boolean correct) {
+        if (correct) {
+            System.out.println(player + " wins $" + win);
+            return win;
+        } else {
+            System.out.println(player + " loses $" + lose);
+            return -lose;
+        }
+    }
+}
